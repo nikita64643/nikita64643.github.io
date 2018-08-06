9425b24e5a716f452222cb11f2f632f551931d72
nikita64643 <nikita64643@yandex.ru>
2018-08-06 17:38:39 +0500

    Начало

---
+++ frontend/js/lol.js
@@ -0,0 +1,30 @@
+function nachalo_show() {
+	$('#nachalo2').hide();
+	$('#nachalo3').hide();
+	$("#nachalo").fadeIn("slow", function() {});
+}
+function nachalo2_show() {
+	$('#nachalo').hide();
+	$('#nachalo3').hide();
+	$("#nachalo2").fadeIn("slow", function() {});
+}
+function nachalo3_show() {
+	$('#nachalo').hide();
+	$('#nachalo2').hide();
+	$("#nachalo3").fadeIn("slow", function() {});
+}
+
+function inventory()
+{prompt("What are you thinking about it?");}
+function history()
+{confirm("Yeah, we're so young, but so ambitious!");}
+function missions()
+{alert("Here are your daily missions. Complete them for your sweet bonus!");
+alert("Just do it!")}
+function achievements()
+{alert("Here are your achievements."); alert("Make it better for your excellence!")}
+function comments()
+{prompt("Write, what things or parts were distressed your");
+alert("Thank you for your precious opinion!");}
+function shop()
+{alert("Please check it later. It's empty yet.");}
